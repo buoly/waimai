8dc7608b1c1f3eaa6c63f5049332cadb40854568
“zhangzhen <zhangzhen@yeah.net>
2018-10-30 15:25:31 +0800

    添加接口

--- src/Meituan/Application.php
+++ src/Meituan/Application.php
@@ -8,5 +8,9 @@ class Application extends ServiceContainer
 {
     protected $providers = [
         Shop\ServiceProvider::class,
+        Shop\ServiceProvider::class,
+        Goods\ServiceProvider::class,
+        Comment\ServiceProvider::class,
+        Activity\ServiceProvider::class,
     ];
 }
